ecbc5bfda61e1157b6f46ec72dfde679c5a672bd
Christopher <bopherchan@gmail.com>
2023-01-31 11:04:09 -0500

    Compl Arith-lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 1
+let num2 = 62
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 14
+let num4 = 10
+const mod = num3 % num4; 
+
+
+const max = Math.max(5, 10, 15, 20)
